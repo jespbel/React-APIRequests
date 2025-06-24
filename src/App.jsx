0470cb126f88535a273ba7f293e181605c9edeee
jespbel <jmbelaso@buffalo.edu>
2025-06-24 12:53:34 -0400

    Created api request, but broke html

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import SearchField from "./componenets/SearchField";
+import GifCard from "./componenets/GifCard";
+
+import "./style.css";
+
 import React from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios"; // Maybe we'll need axios? 🤔
-import "./style.css";
-
-// const GIPHY_API_KEY = "YOUR_API_KEY";
 
 const App = () => {
+  const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState("");
+  const GIPHY_API_KEY = `https://api.giphy.com/v1/gifs/search/${searchText}`;
+
+  // const searchGetRequest = async () => {
+  //   const response = await axios.get(GIPHY_API_KEY)
+  //   if (response.error !== null){
+  //     console.log("Error occured when calling API")
+  //     console.log(response.error)
+  //   }
+  //   else {
+  //     console.log("Data recieved from api request")
+  //     console.log(response.data)
+  //   }
+  // } 
+
   return (
     <div className="app">
-      <h1 className="title">Let's Make Some API Requests!</h1>
+      <h1 className="title">What's the vibe today?</h1>
+      <SearchField id="search-text-box"/>
     </div>
   );
 };
--- src/componenets/SearchField.jsx
+++ src/componenets/SearchField.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import TextField from '@mui/material/TextField';
 
 const SearchField = () => {
-  return <div></div>;
+  return (
+    <div id="search-box" >
+      <TextField 
+        label="Search GIF" 
+        variant="outlined"
+        color="white"/>
+    </div>
+  );
 };
 
 export default SearchField;
